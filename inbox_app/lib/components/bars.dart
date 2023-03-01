a99cce1089f5c79b578d42a80da3cbaa04d4341a
Andrey Demidenko <s2013670@ed.ac.uk>
2023-03-01 16:08:25 +0000

    Start screen done with links to login and register

--- inbox_app/lib/components/bars.dart
+++ inbox_app/lib/components/bars.dart
@@ -1,3 +1,19 @@
-// TODO: AppBar with the name and (?) for help
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget simpleBar(BuildContext context, String text) {
+  return AppBar(
+    backgroundColor: Colors.deepPurple,
+    leading: IconButton(
+      icon: const Icon(
+        Icons.keyboard_arrow_left,
+        size: 42.0,
+        color: Colors.white,
+      ),
+      onPressed: () => Navigator.of(context).pop(),
+    ),
+    title: Text(text),
+    centerTitle: true,
+  );
+}
 
 // TODO: Bottom bar with three icons (deliveries list, home, settings)
--- inbox_app/lib/main.dart
+++ inbox_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:inbox_app/pages/login.dart';
+import 'package:inbox_app/pages/register.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,80 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: StartScreen(),
     );
+  }
+}
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, //Center Column contents vertically,
+            crossAxisAlignment: CrossAxisAlignment
+                .center, //Center Column contents horizontally,
+            children: [
+              const Center(
+                  child: Image(
+                image: AssetImage('assets/img/logo.png'),
+                width: 250,
+                height: 500,
+              )),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(140, 50),
+                    backgroundColor: Colors.deepPurple,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
+                  );
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account yet?',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RegisterScreen()),
+                      );
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                  ),
+                ],
+              ),
+            ]));
   }
 }
