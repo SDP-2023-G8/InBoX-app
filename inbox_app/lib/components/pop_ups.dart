f04ed3f4ddd6edb54cd5a7e633ce176ff3f46c13
Andrey Demidenko <s2013670@ed.ac.uk>
2023-03-02 13:20:09 +0000

    Add pop-up with custom info

--- inbox_app/lib/components/pop_ups.dart
+++ inbox_app/lib/components/pop_ups.dart
@@ -1,5 +1,38 @@
+import 'package:flutter/material.dart';
 // TODO: view/change delivery pop-up widget
 
 // TODO: help pop-ups with different instructions, such as:
 // 1. for registration
 // 2. for usage of the unit
+
+class PopupHelpDialog extends StatelessWidget {
+  final String titleText;
+  final String helpText;
+  const PopupHelpDialog(this.titleText, this.helpText, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(titleText),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(helpText),
+        ],
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+            child: const Text(
+              'Close',
+              style: TextStyle(color: Colors.white),
+            )),
+      ],
+    );
+  }
+}
