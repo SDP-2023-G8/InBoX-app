39505dfaef9e62b2475e2ee96fee56239ee4074e
1adamf <adam.farq@icloud.com>
2023-03-15 10:20:18 +0000

    fixes #7 Adds add delivery button

--- inbox_app/lib/pages/deliveries.dart
+++ inbox_app/lib/pages/deliveries.dart
@@ -24,31 +24,42 @@ class _DeliveriesScreenState extends State<DeliveriesScreen> {
         return true;
       },
       child: GestureDetector(
-          onTap: () {
-            FocusScopeNode currentFocus = FocusScope.of(context);
-            if (!currentFocus.hasPrimaryFocus) {
-              currentFocus.unfocus();
-            }
-          },
-          child: Scaffold(
-              resizeToAvoidBottomInset: false,
-              backgroundColor: Colors.white,
-              appBar: const SimpleBar('Deliveries'),
-              bottomNavigationBar: const BottomBar(0),
-              body: CustomScrollView(slivers: [
-                SliverFillRemaining(
-                  hasScrollBody: false,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Center(
-                        child: Column(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.white,
+          appBar: const SimpleBar('Deliveries'),
+          bottomNavigationBar: const BottomBar(0),
+          body: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Column(
                       children: const [
-                        Text('You have no expected deliveries.')
+                        Text('You have no expected deliveries.'),
                       ],
-                    )),
+                    ),
                   ),
-                )
-              ]))),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {}, //TODO: Implement Add deliveries screen popup
+            backgroundColor: const Color.fromARGB(
+                255, 170, 120, 255), //TODO: Add responsiveness
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ),
     );
   }
 }
