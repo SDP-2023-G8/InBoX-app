23c95ce87e675f1d17d396d941511350b396e448
1adamf <adam.farq@icloud.com>
2023-03-20 11:36:04 +0000

    fixes #7 Implements deliveries screen

--- inbox_app/lib/pages/deliveries.dart
+++ inbox_app/lib/pages/deliveries.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/bars.dart';
+import '../components/delivery.dart';
 import 'homepage.dart';
 
 class DeliveriesScreen extends StatefulWidget {
@@ -10,9 +11,17 @@ class DeliveriesScreen extends StatefulWidget {
 }
 
 class _DeliveriesScreenState extends State<DeliveriesScreen> {
+  var deliveries = <Delivery>[];
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void addDelivery(Delivery delivery) {
+    setState(() {
+      deliveries.add(delivery);
+    });
   }
 
   @override
@@ -35,25 +44,47 @@ class _DeliveriesScreenState extends State<DeliveriesScreen> {
           backgroundColor: Colors.white,
           appBar: const SimpleBar('Deliveries'),
           bottomNavigationBar: const BottomBar(0),
-          body: CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Center(
-                    child: Column(
-                      children: const [
-                        Text('You have no expected deliveries.'),
-                      ],
+          body: Column(
+            children: [
+              Row(
+                children: const [
+                  Text(
+                    'Delivery ID',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Unit ID',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Compartment ID',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Status',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              CustomScrollView(
+                slivers: [
+                  SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Center(
+                        child: Column(children: deliveries),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {}, //TODO: Implement Add deliveries screen popup
+            onPressed: () {
+              addDelivery(Delivery());
+            }, //TODO: Implement Add deliveries screen popup
             backgroundColor: const Color.fromARGB(
                 255, 170, 120, 255), //TODO: Add responsiveness
             child: const Icon(Icons.add),
@@ -63,3 +94,6 @@ class _DeliveriesScreenState extends State<DeliveriesScreen> {
     );
   }
 }
+
+//May Need moved to be a component
+
